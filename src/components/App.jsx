6f01df02ceeaf7cc55f3180f5b1ffe7551093380
Enital <117684114+Enital@users.noreply.github.com>
2023-06-13 17:15:00 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import ImageGallery from './ImageGallery/ImageGallery';
 import SearchBar from './SearchBar/SearchBar';
 import LoadMoreButton from './LoadMoreButton/LoadMoreButton';
@@ -6,72 +6,136 @@ import { api } from './Functions/api';
 
 import './app.module.css';
 
-class App extends React.Component {
-  state = {
-    images: [],
-    query: '',
-    page: 1,
-    load: false,
-    showMoreButton: false,
-  };
+export default function App() {
+  const [images, setImages] = useState([]);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [load, setLoad] = useState(false);
+  const [showMoreButton, setShowMoreButton] = useState(false);
 
-  componentDidUpdate(prevProps, prevState) {
-    const { query, page } = this.state;
-    if (prevState.query !== query || prevState.page !== page) {
-      this.getApiData();
-    }
-  }
+  useEffect(() => {
+    if (!query) return;
 
-  async getApiData() {
-    const {query, page }= this.state;
-    this.setState({ load: true });
-    try {
-      const response = await api(query, page);
-      
-      const { hits: images, totalHits: total } = response;
-
-      if (images.length === 0) {
-        alert('Nothing found =`(');
-        return;
+    const getApiData = async () => {
+      setLoad(true);
+      try {
+        const response = await api(query, page);
+        // console.log(response);
+        const { hits: loadedImages, totalHits: total } = response;
+        if (loadedImages.length === 0) {
+          alert('Nothing found =`(');
+          return;
+        }
+        setShowMoreButton(page < Math.ceil(total / 12));
+        setImages((images) => [...images, ...loadedImages]);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoad(false);
       }
-      const showMoreButton = page < Math.ceil(total / 12);
-      this.setState({ images: [...this.state.images, ...images],
-        showMoreButton });
-    } catch (error) {
-      console.error(error);
-    } finally {
-      this.setState({ load: false });
+    };
+    getApiData();
+  },[query, page]);
+
+  // const handleInputChange = value => {
+  //   this.setState({ query: value });
+  // };
+    
+  const handleFormSubmit = value => {
+    if (value !== query && value !== '') {
+      setImages([]);
+      setQuery(value);
+      setPage(1);
     }
-  }
-
-  handleInputChange = value => {
-    this.setState({ query: value });
   };
-
-  nextPage = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
+  
+  const nextPage = () => {
+    setPage(page => page + 1 );
   };
-
-  handleFormSubmit = value => {
-    value && this.setState({ images: [], query: value, page: 1 });
-  };
-
-  render() {
-    const { images, load, showMoreButton} = this.state;
-
-    return (
+  return (
       <div>
-        <SearchBar onFormSubmit={this.handleFormSubmit}></SearchBar>
+        <SearchBar onFormSubmit={handleFormSubmit}></SearchBar>
           <ImageGallery
             load={load}
             images={images}
-            onClick={this.nextPage}
+            onClick={nextPage}
           ></ImageGallery>
 
-          {showMoreButton &&<LoadMoreButton onClick={this.nextPage} />}
+          {showMoreButton &&<LoadMoreButton onClick={nextPage} />}
       </div>
     );
-  }
-}
+  };
+  
 
-export default App;
+
+// class App extends React.Component {
+//   state = {
+//     images: [],
+//     query: '',
+//     page: 1,
+//     load: false,
+//     showMoreButton: false,
+//   };
+
+//   componentDidUpdate(prevProps, prevState) {
+//     const { query, page } = this.state;
+//     if (prevState.query !== query || prevState.page !== page) {
+//       this.getApiData();
+//     }
+//   }
+
+//   async getApiData() {
+//     const {query, page }= this.state;
+//     this.setState({ load: true });
+//     try {
+//       const response = await api(query, page);
+      
+//       const { hits: images, totalHits: total } = response;
+
+//       if (images.length === 0) {
+//         alert('Nothing found =`(');
+//         return;
+//       }
+//       const showMoreButton = page < Math.ceil(total / 12);
+//       this.setState({ images: [...this.state.images, ...images],
+//         showMoreButton });
+//     } catch (error) {
+//       console.error(error);
+//     } finally {
+//       this.setState({ load: false });
+//     }
+//   }
+
+//   handleInputChange = value => {
+//     this.setState({ query: value });
+//   };
+
+//   nextPage = () => {
+//     this.setState(prevState => ({ page: prevState.page + 1 }));
+//   };
+
+//   handleFormSubmit = value => {
+//     value && this.setState({ images: [], query: value, page: 1 });
+//   };
+
+//   render() {
+//     const { images, load, showMoreButton} = this.state;
+
+//     return (
+//       <div>
+//         <SearchBar onFormSubmit={this.handleFormSubmit}></SearchBar>
+//           <ImageGallery
+//             load={load}
+//             images={images}
+//             onClick={this.nextPage}
+//           ></ImageGallery>
+
+//           {showMoreButton &&<LoadMoreButton onClick={this.nextPage} />}
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+
